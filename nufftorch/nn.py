Finally fixed correct scaling for NUFFT and NUFFT adjoint. TODO: Fix self-adjoint

nufftorch/nn.py
@@ -155,7 +155,7 @@ class NUFFTSelfadjoint(torch.nn.Module):
                  coord: Tensor,
                  shape: Union[int, List[int], Tuple[int]],
                  prep_oversamp: Union[float, List[float], Tuple[float]] = 1.125,
-                 comp_oversamp: Union[float, List[float], Tuple[float]] = 2.0,
+                 comp_oversamp: Union[float, List[float], Tuple[float]] = 1.0,
                  width: Union[int, List[int], Tuple[int]] = 3,
                  basis: Union[Tensor, None] = None,
                  device: Union[str, torch.device] = 'cpu',

nufftorch/src/_factory.py
@@ -66,8 +66,10 @@ class NUFFTFactory(AbstractFactory):
         coord = Utils._scale_coord(coord, shape, oversamp)
 
         # compute interpolator
-        kernel_dict = self._prepare_kernel(
-            coord, grid_shape, width, beta, basis, device)
+        kernel_dict = self._prepare_kernel(coord, grid_shape, width, beta, basis, device)
+        
+        # get scaling
+        scale = Utils._get_kernel_scaling(beta, width)
 
         # device_dict
         device_dict = {'device': device, 'threadsperblock': threadsperblock}
@@ -79,6 +81,7 @@ class NUFFTFactory(AbstractFactory):
                         'width': width,
                         'beta': beta,
                         'kernel_dict': kernel_dict,
+                        'scale': scale,
                         'device_dict': device_dict}
 
         return interpolator
@@ -93,8 +96,7 @@ class NUFFTFactory(AbstractFactory):
         coord = self._reformat_coordinates(coord)
 
         # calculate interpolator
-        kernel_tuple = self._prepare_sparse_coefficient_matrix(
-            coord, shape, width, beta, device)
+        kernel_tuple = self._prepare_sparse_coefficient_matrix(coord, shape, width, beta, device)
 
         # adjust basis
         if basis is not None:
@@ -123,13 +125,11 @@ class NUFFTFactory(AbstractFactory):
 
         for i in range(self.ndim):
             # kernel value
-            empty_value = torch.zeros(
-                (self.coord_length, width[i]), dtype=torch.float32)
+            empty_value = torch.zeros((self.coord_length, width[i]), dtype=torch.float32)
             value.append(empty_value)
 
             # kernel index
-            empty_index = torch.zeros(
-                (self.coord_length, width[i]), dtype=torch.int32)
+            empty_index = torch.zeros((self.coord_length, width[i]), dtype=torch.int32)
             index.append(empty_index)
 
         return value, index
@@ -137,10 +137,8 @@ class NUFFTFactory(AbstractFactory):
     def _reformat_sparse_coefficients(self, value, index, width, device):
         for i in range(self.ndim):
             # reshape
-            value[i] = value[i].reshape(
-                [self.nframes, self.npts, width[i]]).to(device)
-            index[i] = index[i].reshape(
-                [self.nframes, self.npts, width[i]]).to(device)
+            value[i] = value[i].reshape([self.nframes, self.npts, width[i]]).to(device)
+            index[i] = index[i].reshape([self.nframes, self.npts, width[i]]).to(device)
             
         # stack (support isotropic kernel only)
         value = torch.stack(value, dim=0)
@@ -175,9 +173,108 @@ class NUFFTFactory(AbstractFactory):
         return kernel_tuple
 
 
-class AbstractToeplitzFactory(AbstractFactory):
-    """Base class for Cartesian and Non-Cartesian Factory classes."""
+class NonCartesianToeplitzFactory(AbstractFactory):
+    """Functions to prepare Toeplitz kernel for Non-Cartesian sampling."""
+    def __call__(self, coord, shape, prep_osf, comp_osf, width,
+                 basis, device, threadsperblock, dcf):
+        """Actual pre-computation routine."""
+        # clone coordinates and dcf to avoid modifications
+        coord = coord.clone()
+        
+        # kernel precomputation
+        mtf = self._initialize_mtf(coord, shape, width, prep_osf, basis, device, threadsperblock, dcf)
+        
+        # kernel resampling
+        mtf = self._resample_mtf(mtf, shape, prep_osf, comp_osf, width, device)
+        
+        # device_dict
+        device_dict = {'device': device, 'threadsperblock': threadsperblock}
 
+
+        return {'mtf': mtf, 'islowrank': self.islowrank, 'device_dict': device_dict,
+                'ndim': self.ndim, 'oversamp': comp_osf}
+
+    def _initialize_mtf(self, coord, shape, width, prep_osf, basis, device, threadsperblock, dcf):
+        """Core kernel computation sub-routine."""
+        # parse size
+        self._parse_coordinate_size(coord)
+        
+        # process basis
+        basis, basis_adjoint = self._process_basis(basis, device)
+
+        # prepare dcf for MTF/PSF estimation
+        dcf = self._prepare_dcf(dcf, device)
+                
+        # prepare Interpolator object
+        interpolator = NUFFTFactory()(coord, shape, width, prep_osf, basis, device, threadsperblock)
+        
+        return Grid(interpolator['device_dict'])(dcf, interpolator['kernel_dict'])
+
+    def _resample_mtf(self, mtf, shape, prep_osf, comp_osf, width, device):
+        
+        # get arrays
+        width, shape = Utils._scalars2arrays(self.ndim, width, shape)
+
+        # get kernel shape parameter
+        beta = Utils._beatty_parameter(width, prep_osf)
+
+        # IFFT
+        psf = IFFT(mtf)(mtf, axes=range(-self.ndim, 0), norm='ortho')
+        
+        # Crop
+        psf = Crop(shape[-self.ndim:])(psf)
+
+        # Apodize
+        Apodize(shape[-self.ndim:], prep_osf, width, beta, device)(psf)
+
+        # Zero-pad
+        psf = ZeroPad(comp_osf, shape[-self.ndim:])(psf)
+
+        # FFT
+        mtf = FFT(psf)(psf, axes=range(-self.ndim, 0), norm='ortho')
+
+        return mtf
+    
+    def _post_process_mtf(self, mtf):
+
+        # fftshift kernel to accelerate computation
+        mtf = torch.fft.fftshift(mtf, dim=list(range(-self.ndim, 0)))
+
+        if self.islowrank:
+            mtf = mtf.reshape(*mtf.shape[:2], np.prod(mtf.shape[2:]))
+        else:
+            mtf = mtf[:, None, ...]
+
+        # remove NaN
+        if self.isreal:
+            mtf = mtf.real
+            mtf = torch.nan_to_num(mtf)         
+        else:
+            mtf = torch.nan_to_num(mtf.real) + 1j * torch.nan_to_num(mtf.imag)
+            
+        # normalize
+        mtf = mtf / torch.quantile(mtf.flatten().abs(), 0.95)
+
+        # transform to numba
+        if self.islowrank:
+            mtf = BackendBridge.pytorch2numba(mtf)
+
+        return mtf
+    
+    # utils
+    def _prepare_dcf(self, dcf, device):
+        # if dcf are not provided, assume uniform sampling density
+        if dcf is None:
+            dcf = torch.ones((self.nframes, 1, self.npts), torch.complex64, device=device)
+        else:
+            dcf = dcf.clone().squeeze()
+            dcf = dcf.to(torch.complex64).to(device)
+            
+            if len(dcf.shape) == 2:
+                dcf = dcf[:, None, :]
+
+        return dcf
+    
     def _process_basis(self, basis, device):
 
         # inspect basis to determine data type
@@ -203,127 +300,4 @@ class AbstractToeplitzFactory(AbstractFactory):
             basis_adjoint = None
 
         return basis, basis_adjoint
-
-
-    def _post_process_mtf(self, mtf):
-
-        # fftshift kernel to accelerate computation
-        mtf = torch.fft.ifftshift(mtf, dim=list(range(-self.ndim, 0)))
-
-        if self.islowrank:
-            mtf = mtf.reshape(*mtf.shape[:2], np.prod(mtf.shape[2:]))
-        else:
-            mtf = mtf[:, None, ...]
-
-        # remove NaN
-        if self.isreal:
-            mtf = mtf.real
-            mtf = torch.nan_to_num(mtf)         
-        else:
-            mtf = torch.nan_to_num(mtf.real) + 1j * torch.nan_to_num(mtf.imag)
-            
-        # normalize
-        mtf = mtf / torch.quantile(mtf.flatten().abs(), 0.95)
-
-        # transform to numba
-        if self.islowrank:
-            mtf = BackendBridge.pytorch2numba(mtf)
-
-        return mtf
-
-
-class NonCartesianToeplitzFactory(AbstractToeplitzFactory):
-    """Functions to prepare Toeplitz kernel for Non-Cartesian sampling."""
-    def __call__(self, coord, shape, prep_osf, comp_osf, width,
-                 basis, device, threadsperblock, dcf):
-        """Actual pre-computation routine."""
-        # clone coordinates and dcf to avoid modifications
-        coord = coord.clone()
-
-        # get parameters
-        ndim = coord.shape[-1]
-
-        # get arrays
-        width, shape = Utils._scalars2arrays(ndim, width, shape)
-
-        # get kernel shape parameter
-        beta = Utils._beatty_parameter(width, prep_osf)
-
-        # adjust coordinates
-        coord = Utils._scale_coord(coord, shape, prep_osf)
-
-        # device_dict
-        device_dict = {'device': device, 'threadsperblock': threadsperblock}
-
-        # actual kernel precomputation
-        mtf = self._prepare_kernel(coord, shape, prep_osf, comp_osf, width, beta, basis, device_dict, dcf, threadsperblock)
-        
-        return {'mtf': mtf, 'islowrank': self.islowrank, 'device_dict': device_dict,
-                'ndim': ndim, 'oversamp': comp_osf}
-
-    def _prepare_kernel(self, coord, shape, prep_osf, comp_osf, width, beta, basis, device_dict, dcf, threadsperblock):
-        """Core kernel computation sub-routine."""
-        # parse size
-        self._parse_coordinate_size(coord)
-
-        # calculate interpolator
-        mtf = self._prepare_coefficient_matrix(coord, shape, prep_osf, width, basis, device_dict, dcf, threadsperblock)
-
-        # resample mtf
-        mtf = self._resample_mtf(mtf, shape, prep_osf, comp_osf, width, beta, device_dict['device'])
-
-        # post process mtf
-        mtf = self._post_process_mtf(mtf)
-
-        return mtf
-
-    def _prepare_coefficient_matrix(self, coord, shape, oversamp, width, basis, device_dict, dcf, threadsperblock):
-        # process basis
-        basis, basis_adjoint = self._process_basis(basis, device_dict['device'])
-
-        # prepare dcf for MTF/PSF estimation
-        dcf = self._prepare_dcf(dcf, device_dict['device'])
-
-        # calculate modulation transfer function
-        mtf = self._gridding(dcf, coord, shape, width, oversamp, basis, device_dict, threadsperblock)
-        
-        return mtf
-
-    def _prepare_dcf(self, dcf, device):
-        # if dcf are not provided, assume uniform sampling density
-        if dcf is None:
-            dcf = torch.ones((self.nframes, 1, self.npts), torch.complex64, device=device)
-        else:
-            dcf = dcf.clone().squeeze()
-            dcf = dcf.to(torch.complex64).to(device)
-            
-            if len(dcf.shape) == 2:
-                dcf = dcf[:, None, :]
-
-        return dcf
-
-    def _gridding(self, dcf, coord, shape, width, oversamp, basis, device_dict, threadsperblock):
-        # prepare NUFFT object
-        nufft_dict = NUFFTFactory()(coord, shape, width, oversamp, basis, device_dict['device'], threadsperblock)
-
-        # compute mtf
-        return Grid(device_dict)(dcf, nufft_dict['kernel_dict'])
-
-    def _resample_mtf(self, mtf, shape, prep_osf, comp_osf, width, beta, device):
-
-        # IFFT
-        psf = IFFT(mtf)(mtf, axes=range(-self.ndim, 0), norm='ortho')
-        
-        # Crop
-        psf = Crop(shape[-self.ndim:])(psf)
-
-        # Apodize
-        Apodize(shape[-self.ndim:], prep_osf, width, beta, device)(psf)
-
-        # Zero-pad
-        psf = ZeroPad(comp_osf, shape[-self.ndim:])(psf)
-
-        # FFT
-        mtf = FFT(psf)(psf, axes=range(-self.ndim, 0), norm='ortho')
-
-        return mtf
+    

nufftorch/src/_routines.py
@@ -51,6 +51,7 @@ def nufft(image: Tensor, interpolator: Dict) -> Tensor:
     width = interpolator['width']
     beta = interpolator['beta']
     kernel_dict = interpolator['kernel_dict']
+    scale = interpolator['scale']
     device_dict = interpolator['device_dict']
     device = device_dict['device']
 
@@ -62,21 +63,16 @@ def nufft(image: Tensor, interpolator: Dict) -> Tensor:
     image = dispatcher.dispatch(image)
 
     # Apodize
-    print(image.shape)
     Apodize(image.shape[-ndim:], oversamp, width, beta, device)(image)
 
     # Zero-pad
-    print(image.shape)
-    print(oversamp)
     image = ZeroPad(oversamp, image.shape[-ndim:])(image)
 
     # FFT
-    print(image.shape)
-    kdata = FFT(image)(image, axes=range(-ndim, 0), norm='ortho')
+    kdata = FFT(image)(image, axes=range(-ndim, 0), norm=None)
 
     # Interpolate
-    print(kdata.shape)
-    kdata = DeGrid(device_dict)(kdata, kernel_dict)
+    kdata = DeGrid(device_dict)(kdata, kernel_dict) / scale
 
     # Bring back to original device
     kdata, image = dispatcher.gather(kdata, image)
@@ -93,6 +89,7 @@ def nufft_adjoint(kdata: Tensor, interpolator: Dict) -> Tensor:
     width = interpolator['width']
     beta = interpolator['beta']
     kernel_dict = interpolator['kernel_dict']
+    scale = interpolator['scale']
     device_dict = interpolator['device_dict']
     device = device_dict['device']
 
@@ -101,10 +98,10 @@ def nufft_adjoint(kdata: Tensor, interpolator: Dict) -> Tensor:
     kdata = dispatcher.dispatch(kdata)
 
     # Gridding
-    kdata = Grid(device_dict)(kdata, kernel_dict)
+    kdata = Grid(device_dict)(kdata, kernel_dict) / scale
 
     # IFFT
-    image = IFFT(kdata)(kdata, axes=range(-ndim, 0), norm='ortho')
+    image = IFFT(kdata)(kdata, axes=range(-ndim, 0), norm=None)
 
     # Crop
     image = Crop(shape[-ndim:])(image)
@@ -115,13 +112,13 @@ def nufft_adjoint(kdata: Tensor, interpolator: Dict) -> Tensor:
     # Bring back to original device
     image, kdata = dispatcher.gather(image, kdata)
 
-    return image
+    return image * (oversamp**ndim)
 
 
 def prepare_noncartesian_toeplitz(coord: Tensor,
                                   shape: Union[int, List[int], Tuple[int]],
-                                  prep_oversamp: Union[float, List[float], Tuple[float]] = 2,
-                                  comp_oversamp: Union[float, List[float], Tuple[float]] = 2.0,
+                                  prep_oversamp: Union[float, List[float], Tuple[float]] = 1.125,
+                                  comp_oversamp: Union[float, List[float], Tuple[float]] = 1.0,
                                   width: Union[int, List[int], Tuple[int]] = 5,
                                   basis: Union[Tensor, None] = None,
                                   device: Union[str, torch.device] = 'cpu',

nufftorch/test/conftest.py
@@ -3,63 +3,12 @@
 # pylint: disable=too-few-public-methods
 # pylint: disable=too-many-arguments
 
-import itertools
-import pytest
 
 import torch
 
-# %% test parameters
-
-
-def _img_size_list():
-    return [8]
-
-
-def _dim_list():
-    return [2, 3]
-
-
-def _device_list():
-    devices = ['cpu']
-
-    # if torch.cuda.is_available():
-    #     devices.append('cuda')
-
-    return devices
-
-
-def _data_dtype_list():
-    return [torch.complex64]
-
-
-def _coord_type_list():
-    return [torch.float32]
-
-
-def _basis_dtype_list():
-    return [torch.float32, torch.complex64]
-
-
-def _testing_multiple_coils():
-    return [1, 2]
-
-
-def _testing_multiple_echoes():
-    return [1, 2]
-
-
-def _testing_multiple_slices():
-    return [1, 2]
-
-
-def _testing_multiple_frames():
-    return [1, 2]
-
 
 # %% Utils
-
-
-class _Utils:
+class _utils:
 
     @staticmethod
     def normalize(data_in, ndim):
@@ -77,18 +26,14 @@ class _Utils:
         return data_in / scale
 
 
-@pytest.fixture
-def utils():
-    """Wrapper to use Utils as Pytest fixture."""
-    return _Utils
+# %% data generator
+class _kt_space_trajectory:
 
-# %% data generators
+    def __init__(self, ndim, nframes, npix):
 
-
-class _ktSpaceTrajectory():
-
-    def __init__(self, ndim, dtype, nframes, npix):
-
+        # data type
+        dtype = torch.float32
+        
         # build coordinates
         nodes = torch.arange(npix) - (npix // 2)
 
@@ -119,92 +64,36 @@ class _ktSpaceTrajectory():
         self.acquisition_matrix = npix
 
 
-def _image(ndim, device_id, dtype, nframes, nechoes, ncoils, nslices, npix):
+def _kt_space_data(ndim, nframes, nechoes, ncoils, nslices, npix, device):
 
+    # data type
+    dtype = torch.complex64
+
+    if ndim == 2:
+        data = torch.ones((nframes, nechoes, ncoils, nslices, (npix**ndim)), dtype=dtype, device=device)
+
+    elif ndim == 3:
+        data = torch.ones((nframes, nechoes, ncoils, (npix**ndim)), dtype=dtype, device=device)
+
+    return data
+
+
+def _image(ndim, nframes, nechoes, ncoils, nslices, npix, device):
+
+    # data type
+    dtype = torch.complex64
     center = int(npix // 2)
 
     if ndim == 2:
-        img = torch.zeros((nframes, nechoes, ncoils, nslices, npix, npix),
-                          dtype=dtype, device=device_id)
+        img = torch.zeros((nframes, nechoes, ncoils, nslices, npix, npix), dtype=dtype, device=device)
         img[:, :, :, :, center, center] = 1
 
     elif ndim == 3:
-        img = torch.zeros((nframes, nechoes, ncoils, npix, npix, npix),
-                          dtype=dtype, device=device_id)
+        img = torch.zeros((nframes, nechoes, ncoils, npix, npix, npix), dtype=dtype, device=device)
         img[:, :, :, center, center, center] = 1
 
     return img
 
 
-def _kt_space_data(ndim, device_id, dtype, nframes, nechoes, ncoils, nslices, npix):
-
-    if ndim == 2:
-        data = torch.ones((nframes, nechoes, ncoils, nslices, (npix**ndim)),
-                          dtype=dtype, device=device_id)
-
-    elif ndim == 3:
-        data = torch.ones((nframes, nechoes, ncoils, (npix**ndim)),
-                          dtype=dtype, device=device_id)
-
-    return data
-
-
 def _lowrank_subspace_projection(dtype, nframes):
     return torch.eye(nframes, dtype=dtype)
-
-
-# %% parametrized cases
-
-
-def _get_noncartesian_params(lowrank: bool = False,  # pylint: disable=too-many-locals
-                             selfadjoint: bool = False):
-
-    # get input argument combinations
-    args = [_dim_list(),
-            _device_list(),
-            _data_dtype_list(),
-            _coord_type_list(),
-            _testing_multiple_frames(),
-            _testing_multiple_echoes(),
-            _testing_multiple_coils(),
-            _testing_multiple_slices(),
-            _img_size_list()]
-
-    # add lowrank basis if enabled
-    if lowrank is True:
-        args.append(_basis_dtype_list())
-
-    # get combinations
-    args = list(itertools.product(*args))
-
-    # build test data
-    test_data = []
-    
-    for arg in args:
-        ndim = arg[0]
-        device_id = arg[1]
-        data_type = arg[2]
-        coord_type = arg[3]
-        nframes = arg[4]
-        nechoes = arg[5]
-        ncoils = arg[6]
-        nslices = arg[7]
-        npix = arg[8]
-
-        tmp = [ndim, device_id,
-               _image(ndim, device_id, data_type, nframes,
-                      nechoes, ncoils, nslices, npix),
-               _ktSpaceTrajectory(ndim, coord_type, nframes, npix)
-               ]
-        
-        if selfadjoint is False:
-            tmp.append(_kt_space_data(
-                ndim, device_id, data_type, nframes, nechoes, ncoils, nslices, npix))
-            
-        if lowrank is True:
-            basis_type = arg[9]
-            tmp.append(_lowrank_subspace_projection(basis_type, nframes))
-            
-        test_data.append(tmp)
-
-    return test_data

nufftorch/test/test_functional.py
@@ -5,55 +5,88 @@
 # pylint: disable=too-many-arguments
 # pylint: disable=unused-argument
 
-# import pytest
 
-# import torch
+import itertools
+import pytest
 
-# from nufftorch import functional
 
-# from conftest import _get_noncartesian_params
+import torch
 
-# @pytest.mark.parametrize("ndim, device, img, wave, kdata", _get_noncartesian_params())
-# def test_nufft(ndim, device, img, kdata, wave, utils):
+
+from nufftorch import functional
+
+
+from conftest import _kt_space_data, _kt_space_trajectory, _image, _lowrank_subspace_projection
+
+
+# test values
+ndim = [2, 3]
+device = ['cpu']
+dtype = [torch.float32, torch.complex64]
+
+nframes = [1, 2]
+nechoes = [1, 2]
+ncoils = [1, 2]
+nslices = [1, 2]
+
+# if torch.cuda.is_available():
+#     device += ['cuda']
+
+
+# @pytest.mark.parametrize("ndim, nframes, nechoes, ncoils, nslices, device",
+#     list(itertools.product(*[ndim, nframes, nechoes, ncoils, nslices, device])))
+# def test_nufft(ndim, nframes, nechoes, ncoils, nslices, device, npix=8, test=True):
     
 #     # get ground truth
-#     img_ground_truth = img.clone()
-#     kdata_ground_truth = kdata.clone()
+#     img_ground_truth = _image(ndim, nframes, nechoes, ncoils, nslices, npix, device)
+#     kdata_ground_truth = _kt_space_data(ndim, nframes, nechoes, ncoils, nslices, npix, device)
 
 #     # k-space coordinates
+#     wave = _kt_space_trajectory(ndim, nframes, npix)
 #     coord = wave.coordinates
-#     dcf = wave.density_comp_factor.to(kdata.device)
+#     dcf = wave.density_comp_factor.to(kdata_ground_truth.device)
 #     shape = wave.acquisition_matrix
       
 #     # computation
-#     kdata_out = functional.nufft(img, coord=coord, device=device)
-#     img_out = functional.nufft_adjoint(dcf * kdata, coord=coord, shape=shape, device=device)
+#     kdata_out = functional.nufft(img_ground_truth.clone(), coord=coord, device=device)
+#     img_out = functional.nufft_adjoint(dcf * kdata_ground_truth.clone(), coord=coord, shape=shape, device=device)
     
 #     # check
 #     a = torch.inner(img_out.flatten(), img_ground_truth.flatten())
 #     b = torch.inner(kdata_out.flatten(), kdata_ground_truth.flatten())
 
-#     assert torch.allclose(a, b)
+#     if test:
+#       assert torch.allclose(a, b)
+    
+#     return img_out.squeeze().detach(), kdata_out.squeeze().detach()
  
     
-# @pytest.mark.parametrize("ndim, device, img, wave, kdata, basis", _get_noncartesian_params(lowrank=True))
-# def test_nufft_lowrank(ndim, device, img, kdata, wave, basis, utils):
+# @pytest.mark.parametrize("ndim, nframes, nechoes, ncoils, nslices, dtype, device",
+#     list(itertools.product(*[ndim, nframes, nechoes, ncoils, nslices, dtype, device])))
+# def test_nufft_lowrank(ndim, nframes, nechoes, ncoils, nslices, dtype, device, npix=8, test=True):
     
 #     # get ground truth
-#     img_ground_truth = img.clone()
-#     kdata_ground_truth = kdata.clone()
+#     img_ground_truth = _image(ndim, nframes, nechoes, ncoils, nslices, npix, device)
+#     kdata_ground_truth = _kt_space_data(ndim, nframes, nechoes, ncoils, nslices, npix, device)
 
 #     # k-space coordinates
+#     wave = _kt_space_trajectory(ndim, nframes, npix)
 #     coord = wave.coordinates
-#     dcf = wave.density_comp_factor.to(kdata.device)
+#     dcf = wave.density_comp_factor.to(kdata_ground_truth.device)
 #     shape = wave.acquisition_matrix
+    
+#     # get basis
+#     basis = _lowrank_subspace_projection(dtype, nframes)
       
 #     # computation
-#     kdata_out = functional.nufft(img, coord=coord, device=device, basis=basis)
-#     img_out = functional.nufft_adjoint(dcf * kdata, coord=coord, shape=shape, device=device, basis=basis)
+#     kdata_out = functional.nufft(img_ground_truth.clone(), coord=coord, device=device, basis=basis)
+#     img_out = functional.nufft_adjoint(dcf * kdata_ground_truth.clone(), coord=coord, shape=shape, device=device, basis=basis)
     
 #     # check
 #     a = torch.inner(img_out.flatten(), img_ground_truth.flatten())
 #     b = torch.inner(kdata_out.flatten(), kdata_ground_truth.flatten())
 
-#     assert torch.allclose(a, b)
+#     if test:
+#       assert torch.allclose(a, b)
+    
+#     return img_out.squeeze().detach(), kdata_out.squeeze().detach()

nufftorch/test/test_nn.py
@@ -5,119 +5,178 @@
 # pylint: disable=too-many-arguments
 # pylint: disable=unused-argument
 
+
+import itertools
 import pytest
+
 
 import torch
 
+
 from nufftorch import nn
 
-from conftest import _get_noncartesian_params
 
-# @pytest.mark.parametrize("ndim, device, img, wave, kdata", _get_noncartesian_params())
-# def test_nufft(ndim, device, img, kdata, wave, utils):
-    
-#     # get ground truth
-#     img_ground_truth = img.clone()
-#     kdata_ground_truth = kdata.clone()
+from conftest import _kt_space_data, _kt_space_trajectory, _image, _lowrank_subspace_projection, _utils
 
-#     # k-space coordinates
-#     coord = wave.coordinates
-#     dcf = wave.density_comp_factor.to(kdata.device)
-#     shape = wave.acquisition_matrix
-    
-#     # prepare operators
-#     F = nn.NUFFT(coord=coord, shape=shape, device=device)
-#     FH = nn.NUFFTAdjoint(coord=coord, shape=shape, device=device)
+
+# test values
+ndim = [2, 3]
+device = ['cpu']
+dtype = [torch.float32, torch.complex64]
+
+nframes = [1, 2]
+nechoes = [1, 2]
+ncoils = [1, 2]
+nslices = [1, 2]
+
+if torch.cuda.is_available():
+    device += ['cuda']
+
+
+@pytest.mark.parametrize("ndim, nframes, nechoes, ncoils, nslices, device",
+    list(itertools.product(*[ndim, nframes, nechoes, ncoils, nslices, device])))
+def test_nufft(ndim, nframes, nechoes, ncoils, nslices, device, 
+               npix=8, osf=1.125, width=3, test=True):
+        
+    # get ground truth
+    img_ground_truth = _image(ndim, nframes, nechoes, ncoils, nslices, npix, device)
+    kdata_ground_truth = _kt_space_data(ndim, nframes, nechoes, ncoils, nslices, npix, device)
+
+    # k-space coordinates
+    wave = _kt_space_trajectory(ndim, nframes, npix)
+    coord = wave.coordinates
+    dcf = wave.density_comp_factor.to(kdata_ground_truth.device)
+    shape = wave.acquisition_matrix
       
-#     # computation
-#     kdata_out = F(img)
-#     img_out = FH(dcf * kdata)
+    # prepare operators
+    F = nn.NUFFT(coord=coord, shape=shape, device=device, oversamp=osf, width=width)
+    FH = nn.NUFFTAdjoint(coord=coord, shape=shape, device=device, oversamp=osf, width=width)
+      
+    # computation
+    kdata_out = F(img_ground_truth.clone())
+    img_out = FH(dcf * kdata_ground_truth.clone())
     
-#     # check
-#     a = torch.inner(img_out.flatten(), img_ground_truth.flatten())
-#     b = torch.inner(kdata_out.flatten(), kdata_ground_truth.flatten())
+    # check
+    a = torch.inner(img_out.flatten(), img_ground_truth.flatten())
+    b = torch.inner(kdata_out.flatten(), kdata_ground_truth.flatten())
 
-#     assert torch.allclose(a, b)
+    if test:
+      assert torch.allclose(a, b)
+    
+    return img_out.squeeze().detach(), kdata_out.squeeze().detach()
  
     
-# @pytest.mark.parametrize("ndim, device, img, wave, kdata, basis", _get_noncartesian_params(lowrank=True))
-# def test_nufft_lowrank(ndim, device, img, kdata, wave, basis, utils):
-    
+# @pytest.mark.parametrize("ndim, nframes, nechoes, ncoils, nslices, dtype, device",
+#     list(itertools.product(*[ndim, nframes, nechoes, ncoils, nslices, dtype, device])))
+# def test_nufft_lowrank(ndim, nframes, nechoes, ncoils, nslices, dtype, device, npix=8, test=True):
+        
 #     # get ground truth
-#     img_ground_truth = img.clone()
-#     kdata_ground_truth = kdata.clone()
+#     img_ground_truth = _image(ndim, nframes, nechoes, ncoils, nslices, npix, device)
+#     kdata_ground_truth = _kt_space_data(ndim, nframes, nechoes, ncoils, nslices, npix, device)
 
 #     # k-space coordinates
+#     wave = _kt_space_trajectory(ndim, nframes, npix)
 #     coord = wave.coordinates
-#     dcf = wave.density_comp_factor.to(kdata.device)
+#     dcf = wave.density_comp_factor.to(kdata_ground_truth.device)
 #     shape = wave.acquisition_matrix
     
+#     # get basis
+#     basis = _lowrank_subspace_projection(dtype, nframes)
+      
 #     # prepare operators
 #     F = nn.NUFFT(coord=coord, shape=shape, device=device, basis=basis)
 #     FH = nn.NUFFTAdjoint(coord=coord, shape=shape, device=device, basis=basis)
       
 #     # computation
-#     kdata_out = F(img)
-#     img_out = FH(dcf * kdata)
+#     kdata_out = F(img_ground_truth.clone())
+#     img_out = FH(dcf * kdata_ground_truth.clone())
     
 #     # check
 #     a = torch.inner(img_out.flatten(), img_ground_truth.flatten())
 #     b = torch.inner(kdata_out.flatten(), kdata_ground_truth.flatten())
 
-#     assert torch.allclose(a, b)
+#     if test:
+#       assert torch.allclose(a, b)
     
-@pytest.mark.parametrize("ndim, device, img, wave", _get_noncartesian_params(selfadjoint=True))
-def test_nufft_selfadjoint(ndim, device, img, wave, utils):
+#     return img_out.squeeze().detach(), kdata_out.squeeze().detach()
+
+    
+@pytest.mark.parametrize("ndim, nframes, nechoes, ncoils, nslices, device",
+    list(itertools.product(*[ndim, nframes, nechoes, ncoils, nslices, device])))
+def test_nufft_selfadjoint(ndim, nframes, nechoes, ncoils, nslices, device, 
+                           npix=8, osf=1.125, width=3, test=True):
     
     # define tolerance
-    tol = 1e-2  # toeplitz is only approximate
+    tol = 5e-2  # toeplitz is only approximate
+    
+    # get ground truth
+    img = _image(ndim, nframes, nechoes, ncoils, nslices, npix, device)
     
     # k-space coordinates
+    wave = _kt_space_trajectory(ndim, nframes, npix)
     coord = wave.coordinates
     dcf = wave.density_comp_factor.to(img.device)
     shape = wave.acquisition_matrix
     
     # prepare operators
-    F = nn.NUFFT(coord=coord, shape=shape, device=device)
-    FH = nn.NUFFTAdjoint(coord=coord, shape=shape, device=device)    
-    FHF = nn.NUFFTSelfadjoint(coord=coord, shape=shape, device=device, dcf=dcf)
+    F = nn.NUFFT(coord=coord, shape=shape, device=device, oversamp=osf, width=width)
+    FH = nn.NUFFTAdjoint(coord=coord, shape=shape, device=device, oversamp=osf, width=width)    
+    FHF = nn.NUFFTSelfadjoint(coord=coord, shape=shape, device=device, dcf=dcf, prep_oversamp=osf, width=width)
       
     # computation
     img_ground_truth = FH(dcf * F(img.clone()))
     img_toeplitz = FHF(img.clone())
     
     # normalize
-    utils.normalize(img_ground_truth, ndim)
-    utils.normalize(img_toeplitz, ndim)
+    # img_ground_truth = _utils.normalize(img_ground_truth, ndim)
+    # img_toeplitz = _utils.normalize(img_toeplitz, ndim)
     
     # check
     res = torch.norm(img_ground_truth - img_toeplitz) / torch.norm(img_ground_truth)
 
-    assert res < tol
- 
+    if test:
+        assert res < tol
+        
+    return img_ground_truth.squeeze().detach(), img_toeplitz.squeeze().detach(), 
     
-@pytest.mark.parametrize("ndim, device, img, wave, basis", _get_noncartesian_params(lowrank=True, selfadjoint=True))
-def test_nufft_selfadjoint_lowrank(ndim, device, img, wave, basis, utils):
+
+# @pytest.mark.parametrize("ndim, nframes, nechoes, ncoils, nslices, dtype, device",
+#     list(itertools.product(*[ndim, nframes, nechoes, ncoils, nslices, dtype, device])))
+# def test_nufft_selfadjoint_lowrank(ndim, nframes, nechoes, ncoils, nslices, dtype, device, npix=16, test=True):
     
-    # define tolerance
-    tol = 1e-2  # toeplitz is only approximate
+#     # define tolerance
+#     tol = 5e-2  # toeplitz is only approximate
     
-    # k-space coordinates
-    coord = wave.coordinates
-    dcf = wave.density_comp_factor.to(img.device)
-    shape = wave.acquisition_matrix
+#     # get ground truth
+#     img = _image(ndim, nframes, nechoes, ncoils, nslices, npix, device)
     
-    # prepare operators
-    F = nn.NUFFT(coord=coord, shape=shape, device=device, basis=basis)
-    FH = nn.NUFFTAdjoint(coord=coord, shape=shape, device=device, basis=basis)    
-    FHF = nn.NUFFTSelfadjoint(coord=coord, shape=shape, device=device, dcf=dcf, basis=basis)
+#     # k-space coordinates
+#     wave = _kt_space_trajectory(ndim, nframes, npix)
+#     coord = wave.coordinates
+#     dcf = wave.density_comp_factor.to(img.device)
+#     shape = wave.acquisition_matrix
+    
+#     # get basis
+#     basis = _lowrank_subspace_projection(dtype, nframes)
+    
+#     # prepare operators
+#     F = nn.NUFFT(coord=coord, shape=shape, device=device, basis=basis)
+#     FH = nn.NUFFTAdjoint(coord=coord, shape=shape, device=device, basis=basis)    
+#     FHF = nn.NUFFTSelfadjoint(coord=coord, shape=shape, device=device, dcf=dcf, basis=basis)
       
-    # computation
-    img_ground_truth = FH(dcf * F(img.clone()))
-    img_toeplitz = FHF(img.clone())
+#     # computation
+#     img_ground_truth = FH(dcf * F(img.clone()))
+#     img_toeplitz = FHF(img.clone())
     
-    # check
-    res = torch.norm(img_ground_truth - img_toeplitz) / torch.norm(img_ground_truth)
+#     # normalize
+#     img_ground_truth = _utils.normalize(img_ground_truth, ndim)
+#     img_toeplitz = _utils.normalize(img_toeplitz, ndim)
+    
+#     # check
+#     res = torch.norm(img_ground_truth.abs() - img_toeplitz.abs()) / torch.norm(img_ground_truth.abs())
 
-    assert res < tol
-
+#     if test:
+#         assert res < tol
+    
+#     return img_ground_truth.squeeze().detach(), img_toeplitz.squeeze().detach()
+ 